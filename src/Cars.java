1dfc786288d4334a56f2789dc2f6bb329880d7b5
idergunov <idergunov058@yandex.ru>
2017-03-16 00:11:50 +0300

    Добавлены классы: Garage, SparePats.

--- src/Cars.java
+++ src/Cars.java
@@ -9,5 +9,8 @@ public class Cars {
         Engine engine_v8 = new Engine(2003);
         engine_v8.PrintInfo();
 
+        Engine engine_v6 = new Engine(800);
+        engine_v6.PrintInfo();
+
     }
 }
